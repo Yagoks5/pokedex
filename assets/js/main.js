ed675ccffc426fcbdcff21386cb8779b28749aad
Yago <yago.ks5@gmail.com>
2023-10-02 18:19:25 -0300

    Transformando a lista de pokemons API em uma lista de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,33 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 //nos retorna uma promise -> uma promessa de resposta
+
+function convertPokemonToLi(pokemon) {
+  return `
+  <li class="pokemon">
+          <span class="number">#001</span>
+          <span class="name">${pokemon.name}</span>
+          <div class="detail">
+            <ol class="types">
+              <li class="type">grass</li>
+              <li class="type">poison</li>
+            </ol>
+            <img
+              src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+              alt="${pokemon.name}"
+            />
+          </div>
+        </li>
+      `;
+}
+const pokemonList = document.getElementById("pokemonList");
 fetch(url) // -> usa o get
   .then((response) => response.json())
-  .then((jsonBody) => console.log(jsonBody))
-  .catch((error) => console.log(error))
-  .finally(() => {
-    console.log("Requisição concluida");
-  });
-console.log("pudim");
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToLi(pokemon);
+    }
+  })
+  .catch((error) => console.log(error));
