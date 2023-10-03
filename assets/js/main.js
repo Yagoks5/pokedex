d055d2ab61455097eb19d9ef3fc935147f68cc88
Yago <yago.ks5@gmail.com>
2023-10-03 09:10:51 -0300

    Adicionando requisições em paralelo para nome, tipo e imagem de cada pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,23 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 //nos retorna uma promise -> uma promessa de resposta
 
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map((typeSlot) => {
+    return `<li class="type">${typeSlot.type.name}</li>`;
+  });
+}
+
 function convertPokemonToLi(pokemon) {
   return `
   <li class="pokemon">
-          <span class="number">#001</span>
+          <span class="number">#${pokemon.order}</span>
           <span class="name">${pokemon.name}</span>
           <div class="detail">
             <ol class="types">
-              <li class="type">grass</li>
-              <li class="type">poison</li>
+              ${convertPokemonTypesToLi(pokemon.types).join("")}
             </ol>
             <img
-              src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+              src="${pokemon.sprites.other.dream_world.front_default}"
               alt="${pokemon.name}"
             />
           </div>
